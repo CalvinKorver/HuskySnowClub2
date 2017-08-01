82e1492eab384d7f875c72c28752e8c8d3b157dc
Calvin <calvin.korver@gmail.com>
2017-08-01 10:11:38 -0500

    Got stuff working

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
 <?php
+<<<<<<< HEAD
+ 
+require 'vendor/autoload.php';
+ 
+use App\SQLiteConnection;
+use App\SQLiteInsert;
+ 
+$pdo = (new SQLiteConnection())->connect();
+$sqlite = new SQLiteInsert($pdo);
+
+$fname = $_POST['interest-fname'];
+$lname = $_POST['interest-lname'];
+$email = $_POST['interest-email'];
+if (isset($_POST['interest_class'])) {
+    $interest = $_POST['interest_class'];
+
+    echo 'selected class: ';
+    echo $interest . '  ';
+}
+$buttonType = $_POST['button-type'];
+
+if ($buttonType == 'interest') {
+    $res = $sqlite->getEmailCount($email);
+    if ($res == 0) {
+        $memberId = $sqlite->insertMemberInterest($fname, $lname, $email, $class);
+        header("Location: interest-success.html");
+    } else {
+        header("Location: interest-error.html");
+    }
+}
+ 
+
+=======
 require 'vendor/autoload.php';
 echo 'Running index.php';
 use App\SQLiteConnection;
@@ -16,3 +49,4 @@ else
 	$sqlite = new SQLiteInsert($pdo);
 	
 	$sqlite->insertInterest('test1','test1','test1','test1');
+>>>>>>> 1eefe2ea1d88b163d097b6c01cfc9afade6e6b69
