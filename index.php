000b401966202a323583dd124ee35307a6c6b80e
Calvin <calvin.korver@gmail.com>
2017-09-14 11:02:22 -0700

    task/fixCashError code error page new links w/o html

--- index.php
+++ index.php
@@ -52,22 +52,33 @@ if ($buttonType == 'admin') {
         if ((isset($_POST['activity']) && $_POST['activity'] != null) ? $activity = $_POST['activity'] : $canSignup = false);
 
         /* Checks if cashcode is correct if user selected cash as payment method */
-        if ($payment == 'cash' && isset($_POST['cashcode'])) {
+        if ($payment == 'cash') {
             $cashcode = $_POST['cashcode'];
-            $canSignup = $sqlite->checkCashCode($cashcode);
+            if ($sqlite->checkCashCode($cashcode) != 1) { 
+                $canSignup = false;
+                $cashError = true;
+            }
         }
 
         /* Checks if email exists*/
         $emailExists = $sqlite->memberExists($email, 'signup');
+        
+        // Case 1: We can insert a new member barring any database insertion errors
         if (!$emailExists && $canSignup) {
             $memberId = $sqlite->insertMemberSignup($fname, $lname, $email, $class, $refer, $activity, $payment);
             if ($memberId != null ) {
-                header("Location: pages/success-signup.html");
+                header("Location: pages/success-signup");
             } else {
-                header("Location: pages/fail.html");
+                header("Location: pages/error");
             }
+
+        // Case 2: There was a cash code error
+        } else if ($cashError) {
+            header("Location: pages/passerror");
+
+        // Case 3: There was a general failure, the email likely already exists in the database.
         } else {
-            header("Location: pages/fail.html");
+            header("Location: pages/fail");
         }
     }
 }
